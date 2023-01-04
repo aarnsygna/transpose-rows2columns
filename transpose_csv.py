7578720451d44444b32051068d78b552670fa12d
aarnsygna <aaron.ruiz@sygnamap.com>
2023-01-04 14:11:19 -0600

    Functionality added to transpose_csv.py
    Added the pandas module and functionality to read csv
    csv file is read and transposed.

--- transpose_csv.py
+++ transpose_csv.py
@@ -1,2 +1,13 @@
 import pandas as pd
 
+# None_RCC_17-635-28_N1_30um_S1_20200920_pixel_intensities copy.csv
+csv_filename = input("Enter .csv file to read: ")
+
+#csv_df = pd.read_csv(filepath_or_buffer=csv_filename).transpose()
+csv_df = pd.read_csv(filepath_or_buffer=csv_filename,
+                     sep=",", header=2,
+                     index_col=0
+                     ).transpose()
+
+
+print(csv_df.head())
